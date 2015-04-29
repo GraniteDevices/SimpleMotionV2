5e1d1035cd3389f928c5e734ba6b18e075530c7c
Tero K <tero.k@granitedevices.fi>
2015-04-29 13:36:18 +0300

    Change name of SM_SIMWHEEL_MODE to SMP_TORQUE_MODE_ATTRIBUTES

--- simplemotion_defs.h
+++ simplemotion_defs.h
@@ -571,14 +571,16 @@
 #define SMP_DYNAMIC_BRAKING_SPEED 912
 #define SMP_BRAKE_STOP_ENGAGE_DELAY SMP_MECH_BRAKE_ENGAGE_DELAY /* SMP_BRAKE_STOP_ENGAGE_DELAY is old name, kept for compatibility*/
 
-/*Racing simulator wheel torque control specific settings, currently IONI only. Values:
+/*Special torque mode specific settings, currently IONI only. Values:
  * 0 = disabled, standard drive operation
  * nonzero = calculate value by:
  *    round(allowed_rotation_in_counts/100)*100 + mode
  *
  *    where mode:
  *    	0=only rotation limit on
- *    	1=rotation limit and analog trimpot torque scaling on. Analog input 0-5V sets torque scale to 10-100%
+ *    	1=rotation limit and analog trimpot torque scaling on.
+ *    		Analog input 0-5V sets torque scale to 10-100%. Note: cant use analog input for torque setpoint now.
+ *    	2-99=reserved for future use
  *
  *   example 1: we want rotation limit of 8192 encoder counts (for each direction) and no scaling input, we
  *   use value 8200.
@@ -589,7 +591,7 @@
  *   Tip: use Granity Ctrl+P (Shortcut) dialog to set value:
  *   http://granitedevices.com/wiki/Reading_and_writing_an_arbitrary_parameter_with_Granity
  */
-#define SMP_SIMWHEEL_MODE 920
+#define SMP_TORQUE_MODE_ATTRIBUTES 920
 
 //////////////////////////////////////////////////////////////////////////////////RUNTIME PARAMS 5000-9999
 
