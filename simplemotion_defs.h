f357575b3ac2dc9caaf5c9673f0c74c4b7f87d91
Tero Kontkanen <tero.kontkanen@granitedevices.fi>
2017-05-07 21:51:57 +0300

    - Renamed scope param 5014 as SMP_CAPTURE_BEFORE_TRIGGER_SAMPLE_COUNT - Added support for external trigger input for capture

--- simplemotion_defs.h
+++ simplemotion_defs.h
@@ -710,12 +710,13 @@
 	#define TRIG_TARGETCHANGE 3
 	#define TRIG_TARGETCHANGE_POS 4
 	#define TRIG_SERIALCMD 5
+	#define TRIG_EXTERNAL_INPUT 6
 
 #define SMP_CAPTURE_SAMPLERATE 5012
 //rdonly
 #define SMP_CAPTURE_BUF_LENGHT 5013
-//SMP_CAPTURE_TRIGGER_DELAY sets scope capture trigger delay. Value 0 is traditional, +n starts capture n sample cycles before trigger, -n after trigger. In positive delay, the maximum effective value is SMP_CAPTURE_BUF_LENGHT divided by number of channels selected.
-#define SMP_CAPTURE_TRIGGER_DELAY 5014
+//SMP_CAPTURE_BEFORE_TRIGGER_SAMPLE_COUNT sets how much samples will be preserved before trigger event. Value 0 is traditional, +n starts capture n sample cycles before trigger, -n after trigger. In positive delay, the maximum effective value is SMP_CAPTURE_BUF_LENGHT divided by number of channels selected.
+#define SMP_CAPTURE_BEFORE_TRIGGER_SAMPLE_COUNT 5014
 //this is looped 0-n to make samples 0-n readable from SMP_CAPTURE_BUFFER_GET_VALUE
 #define SMP_CAPTURE_BUFFER_GET_ADDR 5333
 #define SMP_CAPTURE_BUFFER_GET_VALUE 5334
@@ -738,6 +739,7 @@
 	#define DEVICE_CAPABILITY_SENSORLESS_COMMUTATION BV(8)
 	#define DEVICE_CAPABILITY_ANALOG_OUTPUT BV(9)
 	#define DEVICE_CAPABILITY_SCOPE_TRIGGER_DELAY BV(10)
+	#define DEVICE_CAPABILITY_SCOPE_EXTERNAL_TRIGGER BV(11)
 
 #define SMP_FIRMWARE_VERSION 6010
 #define SMP_FIRMWARE_BACKWARDS_COMP_VERSION 6011
